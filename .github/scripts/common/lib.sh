b8cb7a03fafb50f632ed3d71b5134b74565973a9
EgorPopelyaev <egor@parity.io>
2023-11-10 14:46:19 +0100

    debug function

--- .github/scripts/common/lib.sh
+++ .github/scripts/common/lib.sh
@@ -308,6 +308,7 @@ function increment_rc_tag() {
 # (going to be either pokadot or polkadot-parachain)
 function get_product_name() {
 	current_branch=$(git branch --show-current)
-	[[ "$current_branch" =~ "release-polkadot" ]] && echo "polakdot"
-	[[ "$current_branch" = "release-cumulus"  ]] && echo "polakdot-parachain"
+	[[ "$current_branch" =~ "release-polkadot" ]] && product="polakdot"
+	[[ "$current_branch" =~ "release-cumulus"  ]] && product="polakdot-parachain"
+  echo $product
 }
